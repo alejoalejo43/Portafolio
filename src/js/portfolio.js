ff5fbb2a057d60e714042556fef16897788e690d
alejoalejo43 <alejandrocastillo.v@hotmail.com>
2020-10-21 18:03:10 -0500

    portfolio page ok

--- src/js/portfolio.js
+++ src/js/portfolio.js
@@ -8,3 +8,67 @@ require('./../img/house-5.jpeg');
 require('./../img/jeans.jpg');
 require('./../img/alpunto2.jpg');
 require('./../img/mycity.jpg');
+const pack = {
+  infoPack: [
+    {
+      name: 'Podcast',
+      image: 'static/eniac.png',
+      text: 'Práctica podcast',
+    },
+    {
+      name: 'Omnifood',
+      image: 'static/food.jpg',
+      text: 'Práctica Omnifood',
+    },
+    {
+      name: 'Natours',
+      image: 'static/hero-small.jpg',
+      text: 'Práctica Natours',
+    },
+    {
+      name: 'Triller',
+      image: 'static/hotel-2.jpg',
+      text: 'Práctica Triller',
+    },
+    {
+      name: 'Nexter',
+      image: 'static/house-5.jpeg',
+      text: 'Práctica Nexter',
+    },
+    {
+      name: 'dominico',
+      image: 'static/jeans.jpg',
+      text: 'Tienda Dominico',
+    },
+    {
+      name: 'Alpunto',
+      image: 'static/alpunto2.jpg',
+      text: 'Periódico AL Punto',
+    },
+  ],
+};
+
+function sum(acc, val) {
+  return (
+    acc +
+    `
+  <div class="brochure__index--card">
+    <img src= ${val.image} alt=${val.name} class="brochure__index--img" />
+    <div class="brochure__index--container">
+      <p>${val.text}</p>
+      <button class="button button__red button__small button__small--brochure">
+        <a href="#">Ver proyecto</a>
+      </button>
+    </div>
+  </div>
+  `
+  );
+}
+
+const markup = pack.infoPack.reduce(sum, '');
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  document
+    .querySelector('.brochure__index')
+    .insertAdjacentHTML('afterbegin', markup);
+});
